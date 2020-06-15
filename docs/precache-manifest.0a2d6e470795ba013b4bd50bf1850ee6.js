282db94e44292dc1f6f84458b1f5a286e9159ca1
beatriz-perez <beatriz.pfp@gmail.com>
2020-06-15 10:18:10 +0200

    fix home

---
+++ docs/precache-manifest.0a2d6e470795ba013b4bd50bf1850ee6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2720338aa8e21b90eaf3292305547a21",
+    "url": "/index.html"
+  },
+  {
+    "revision": "090493b1e01745eede52",
+    "url": "/static/css/main.841eeca0.chunk.css"
+  },
+  {
+    "revision": "2e89275f2ee897f7e89c",
+    "url": "/static/js/2.6e01b56b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.6e01b56b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "090493b1e01745eede52",
+    "url": "/static/js/main.f8662706.chunk.js"
+  },
+  {
+    "revision": "24e8d5a84308de897e3a",
+    "url": "/static/js/runtime-main.c8f0954f.js"
+  }
+]);
